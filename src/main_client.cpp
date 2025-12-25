13c2fdbc9a117658e4c16bdfed01ffd3c075e60e
malakelbanna <malakelbanna2004@gmail.com>
2025-12-25 13:00:53 +0200

    add retry logic

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -9,8 +9,24 @@ int main(int argc, char **argv)
 
     std::string server_list = "ipv4:127.0.0.1:50051,127.0.0.1:50052";
 
+    std::string service_config =
+        "{"
+        "  \"methodConfig\": [{"
+        "    \"name\": [{}],"
+        "    \"retryPolicy\": {"
+        "      \"maxAttempts\": 3,"
+        "      \"initialBackoff\": \"0.1s\","
+        "      \"maxBackoff\": \"1s\","
+        "      \"backoffMultiplier\": 2,"
+        "      \"retryableStatusCodes\": [\"UNAVAILABLE\"]"
+        "    }"
+        "  }]"
+        "}";
+
     grpc::ChannelArguments args;
     args.SetLoadBalancingPolicyName("round_robin");
+
+    args.SetServiceConfigJSON(service_config);
 
     auto channel = grpc::CreateCustomChannel(
         server_list,
