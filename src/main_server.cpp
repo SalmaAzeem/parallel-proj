1aa98c6d3777df91bcd6054b12337cb4e9db1f1a
Alhusseain Shalaby <80164892+alhusseain@users.noreply.github.com>
2025-12-27 22:22:43 +0200

    Update main_server.cpp

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -39,6 +39,9 @@ class FractalServiceImpl final : public fractal::FractalService::Service
 
 int main()
 {
+    std::cout<<"delaying .....\n";
+    sleep(5);
+    std::cout<<"server starting\n";
     std::string server_address("0.0.0.0:50051");
     FractalServiceImpl service;
 
